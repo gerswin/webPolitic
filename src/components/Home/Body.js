5721e5e7ce600bb6bc2605e2b39ac0f6ebf96f56
mayerson18 <mayerson18@gmail.com>
2019-08-15 18:13:26 -0500

    home and activity

--- src/components/Home/Body.js
+++ src/components/Home/Body.js
@@ -1,37 +1,49 @@
 import React, {Component} from "react";
 import {withAlert} from 'react-alert'
 import colors from "../../globals/colors";
-import {Col, Container, Jumbotron, Row} from 'reactstrap';
+import {Col, Row} from 'reactstrap';
 import Link from './Link'
+import imgRetos from "../../assets/retosIcon.png";
+import imgActividades from "../../assets/actividadIcon.png";
+import { Route } from 'react-router-dom'
 
 class Body extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: [
-        {
-          text: '-Suma 5 Activistas a tu estructura',
-        },
-        {
-          text: '-Nuevo Reto',
-        },
-        {
-          text: '-Suma 10 movilizadores a tu estructura',
-        },
-      ]
+      
     };
   }
 
-  renderHome() {
+  renderItemHome(text, logo, url) {
+    return <Route render={({ history}) => (
+      <div
+        style={styles.containerItem}
+        onClick={() => history.push(`${url}`)}
+      >
+        <img 
+          src={logo}
+          alt="#"
+          style={{ width: 150, height: 100 }}
+        />
+        <p style={styles.itemText}>{text}</p>
+      </div>
+    )} />
+  }
 
+  renderHome() {
+    return <div>
+      {this.renderItemHome('Retos de la semana', imgRetos, '/profile')}
+      {this.renderItemHome('Actividades semanales', imgActividades, '/activity')}
+    </div>
   }
 
   renderChallenges() {
-    return this.state.list.map((item, index) => {
+    return this.props.list.map((item, index) => {
       return <Link
         text={item.text}
         key={index}
-        isLast={index === this.state.list.length-1}
+        isLast={index === this.props.list.length-1}
       />
     })
   }
@@ -39,7 +51,7 @@ class Body extends Component {
   typeRender() {
     if (this.props.type === 'home') {
       return this.renderHome()
-    } else if (this.props.type === 'challenges') {
+    } else {
       return this.renderChallenges()
     }
   }
@@ -52,7 +64,6 @@ class Body extends Component {
             <h2 style={styles.title}>
               {this.props.title}
             </h2>
-            <div ></div>
             { this.typeRender() }
           </div>
         </Col>
@@ -68,5 +79,17 @@ const styles = {
     marginTop: '1em',
     color: colors.green,
     textAlign: 'center'
+  },
+  containerItem: {
+    cursor: 'pointer',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  itemText: {
+    color: colors.blue,
+    fontSize: 19,
+    fontWeight: 'bold'
   }
 }
