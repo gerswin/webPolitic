cc21b9cf1781f13299270843c7d298629f4961bb
Gerswin Lee <g3rswin@gmail.com>
2019-08-16 02:06:00 -0500

    secure

--- src/App.js
+++ src/App.js
@@ -83,11 +83,11 @@ const HomeApp = ({ match }) => {
           <div className="container">
             <Route exact path="/" component={Login} />
             <Route exact path="/login" component={Login} />
-            <Route exact path="/profile" component={Profile} />
+            <PrivateRoute exact path="/profile" component={Profile} />
             <PrivateRoute exact path="/home" component={Home} />
-            <Route exact path="/activity" component={Activity} />
+            <PrivateRoute exact path="/activity" component={Activity} />
             <Route exact path="/signup" component={Signup} />
-            <Route exact path="/challenge/:id" component={Challenge} />
+            <PrivateRoute exact path="/challenge/:id" component={Challenge} />
             <PrivateRoute exact path="/search" component={Search} />
             <PrivateRoute exact path="/join" component={RegisterForm} />
 
