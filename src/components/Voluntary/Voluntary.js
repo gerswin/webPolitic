e1d981b100a6d26bbfef52aae4ed59b2cd3e8e59
Gerswin Lee <g3rswin@gmail.com>
2019-08-16 18:06:35 -0500

    fix user parent

--- src/components/Voluntary/Voluntary.js
+++ src/components/Voluntary/Voluntary.js
@@ -100,7 +100,7 @@ class Voluntary extends Component {
                                     client: parent,
                                     name: name,
                                     cc: cc,
-                                    location: location[0].value,
+                                    location: location[0].value === undefined ? "" :  location[0].value,
                                     email: email.toLowerCase().trim(),
                                     phone: phone,
                                     parent: userEmail,
@@ -287,7 +287,7 @@ class Voluntary extends Component {
 }
 
 const mapStateToProps = state => ({
-    userEmail: state.userInfo.email,
+    userEmail: state.email
 });
 
 export default compose(
