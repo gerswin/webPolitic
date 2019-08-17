7ae9594e06da991f06e5b69e149b3e9294e9884d
Gerswin Lee <g3rswin@gmail.com>
2019-08-17 09:00:55 -0500

    fix text

--- src/components/Home/Avatar.js
+++ src/components/Home/Avatar.js
@@ -85,6 +85,7 @@ class Avatar extends Component {
       default:
         roleName = "Admin";
     }
+    const {userCount,userNetCount} = this.props;
     return (
       <Row style={styles.margin}>
         <Col >
@@ -116,8 +117,8 @@ class Avatar extends Component {
           <h4 style={styles.title}>{this.props.userName}</h4>
           <div style={styles.subtitle}>
             <p style={styles.p}>{roleName}</p>
-            <p style={styles.p}>{this.props.userCount} {this.props.userCount > 1 ? "Registros" : "Registro"} </p>
-            <p style={styles.p}>{this.props.userNetCount} {this.props.userNetCount > 1 ? "Voluntarios" : "Voluntario"} en Red</p>
+            <p style={styles.p}>{userCount} {userCount > 1 ? "Registros" : userCount === 0 ?  "Registros" :  "Registro" } </p>
+            <p style={styles.p}>{userNetCount} {userNetCount > 1 ? "Voluntarios" : userCount === 0 ?  "Voluntarios" :  "Voluntario"  } en Red</p>
             <Route render={({ history}) => (
                 <button
                     className="btn btn-primary btn-block mg-b-10"
